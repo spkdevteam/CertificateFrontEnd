09a01f8352094ac2c47a9dc28bc916db9224643c
spkDevTeam <spkdevteam@gmail.com>
2025-02-19 18:30:08 +0530

    added ci implementation

--- src/components/ViewCertificate/CerticateView.jsx
+++ src/components/ViewCertificate/CerticateView.jsx
@@ -1,14 +1,9 @@
 import { useEffect } from "react"
-import useCertificatehook from "../../Hooks/useCertificateHooks"
-
- 
-
-const CerticateView = ({value})=>{
-    
-    useEffect(()=>{
-        console.log(value,'selectedCertificateselectedCertificateselectedCertificate')
-    },[value])
-    return(
+const CerticateView = ({ value }) => {
+    useEffect(() => {
+        console.log(value, 'selectedCertificateselectedCertificateselectedCertificate')
+    }, [value])
+    return (
         <div className="w-full h-full">
             {console.log(value?.certificateNumber)}
             {value?.certificateNumber}
