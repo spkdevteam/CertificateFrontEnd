5a37452f927521c2790c3d9e07ec141c5700fb13
Abhisekkaran86 <abhisekkaran86@gmail.com>
2025-02-20 18:27:41 +0530

    painscaleadded

--- src/components/ViewCertificate/SearchCertificate.jsx
+++ src/components/ViewCertificate/SearchCertificate.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import SPKBTNSave from "../../common/Button/SPKBTNSave"
 import SPKInputText from "../../common/Input/SPKInputText"
 import useCertificatehook from "../../Hooks/useCertificateHooks"
+import PainScale from "../painScale/painscale"
 
 
 const SearchCertificate = ({onChange}) => {
@@ -14,6 +15,10 @@ const SearchCertificate = ({onChange}) => {
  
     return (
         <div className="w-full h-full flex justify-end items-end  gap-4 p-4 ">
+           <div className="h-10 w-1/2">
+           <PainScale startRange={10} endRange={20} value={12} name="pain"/> 
+           </div>
+
             <div className='flex flex-col gap-2'>
                 <label className='text-gray-700 font-medium'>Certificate Number</label>
                 <SPKInputText value={certificateNUmber} onChange={(e)=>setCertificateNUmber(e?.target?.value)} name={'certificateNumber'} placeHolder={'Certificate'} className='border  p-2  ' />
